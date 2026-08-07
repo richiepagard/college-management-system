7673675bd0bb19f6b5ef236cd194a00b41cd81f1
richie <richiepagard@gmail.com>
2026-08-07 18:15:38 -0400

    init; Initialize the django project and config the base settings.

---
+++ college/core/settings/conf/apps_conf.py
@@ -0,0 +1,10 @@
+DEFAULT_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+INSTALLED_APPS = DEFAULT_APPS
