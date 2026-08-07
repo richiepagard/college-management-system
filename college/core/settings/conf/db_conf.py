146a9009475832d5b44b71ba187a0f0576e3ea37
richie <richiepagard@gmail.com>
2026-08-07 18:22:15 -0400

    feat; Add python-dotenv and config DB for PostgreSQL.

--- college/core/settings/conf/db_conf.py
+++ college/core/settings/conf/db_conf.py
@@ -1,8 +1,15 @@
-from settings.base import BASE_DIR
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
