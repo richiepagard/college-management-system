dbb460cd5662cdc5d9686cdbf9dd76ae676b4d77
richie <richiepagard@gmail.com>
2026-08-10 15:47:45 -0400

    feat; Implement User Profile Signal.
    - Implement a new signal in accounts app to create a profile for the created user suddenly after user created successfully.

---
+++ college/apps/accounts/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import User, UserProfile
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Create a new insance of User Profile if the new user created
+    successfully. After user object created, a new empty User Profile
+    instance creates with the default 'user' field to the created user. 
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance ,**kwargs):
+    """
+    Updates the created 'User' instance by saving
+    its Profile.
+    """
+    instance.profile.save()
