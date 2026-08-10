4e6cd765c7ef52f4ef8189445cd158261c1bf870
richie <richiepagard@gmail.com>
2026-08-10 15:23:40 -0400

    feat; Config Media Files In Settings and Root URLs.

--- college/core/settings/base.py
+++ college/core/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,6 +84,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Email
