620b443c5ae43a0097dfd62c7488c7986b4abcba
richie <richiepagard@gmail.com>
2026-08-18 20:20:20 -0400

    feat; Implement Student Model.
    - Implement Student model which contains the related User (O2O relation) and Student Unique Code to identify it by its code and it set as DB Index.
    
    - Override the model `save` method to set a unique code for Student Code. Uses the `unique_code_generator` tool with 10 length.

--- college/apps/students/models.py
+++ college/apps/students/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from apps.common.models import BaseModel
+from apps.common.utils import unique_code_generator
+
+User = get_user_model()
+
+
+class Student(BaseModel):
+    """
+    Represents to the Student's in the college.
+    Each student is like a user with Student role.
+
+    Attributes:
+        user (int, O2O): The student's user. Implies to the user and its profile.
+        student_code (str): Student's unique code with '10' length to identify each student by the code.
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="student",
+        null=True,
+        verbose_name=_("User")
+    )
+    student_code = models.CharField(
+        max_length=11,
+        unique=True,
+        db_index=True,
+        blank=True,
+        verbose_name=_("Student Code")
+    )
+
+    class Meta:
+        verbose_name = _("Student")
+        verbose_name_plural = _("Students")
+
+    def __str__(self) -> str:
+        return f"{self.user.phone_number} - {self.student_code}"
+
+    def save(self, *args, **kwargs) -> None:
+        """
+        Overriding the save method.
+        If the target object is new, then generates an unique code
+        for the student's Student Code field.
+        """
+        is_new = self.pk is None
+
+        if is_new:
+            self.student_code = unique_code_generator(10)
+
+        super().save(*args, **kwargs)
