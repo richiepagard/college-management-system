a38f2ae1c9b76aca64c85f11014da800c7ecdc4b
richie <richiepagard@gmail.com>
2026-08-19 23:07:20 -0400

    test; Test Cases For Unique Code Generator Tool.
    - Wrote test cases for various scenarios of `Unique Code Generator` tool which generates a unique code with the provided length that it default value is `10`.

--- college/apps/common/tests/test_utils.py
+++ college/apps/common/tests/test_utils.py
@@ -2,7 +2,7 @@ import os
 
 from django.test import SimpleTestCase
 
-from apps.common.utils import path_with_hash
+from apps.common.utils import path_with_hash, unique_code_generator
 
 
 class PathWithHashTestCase(SimpleTestCase):
@@ -43,3 +43,44 @@ class PathWithHashTestCase(SimpleTestCase):
         second = path_with_hash("profile.jpg")
 
         self.assertNotEqual(first, second)
+
+
+class UniqueCodeGeneratorTestCase(SimpleTestCase):
+
+    def test_return_string_digit_ensure(self):
+        """
+        Tests to ensure that the return string from
+        the 'unique_code_generator' method is digit.
+        """
+        result = unique_code_generator(6)
+
+        self.assertTrue(result.isdigit())
+        self.assertFalse(result.isalpha())
+
+    def test_correct_length_return(self):
+        """
+        Tests that the length of return string is exactly
+        same as what passed to.
+        """
+        result = unique_code_generator(6)
+
+        self.assertEqual(len(result), 6)
+
+    def test_code_uniqueness(self):
+        """
+        Tests that the generated code is unique.
+        """
+        result1 = unique_code_generator(4)
+        result2 = unique_code_generator(4)
+
+        self.assertNotEqual(result1, result2)
+        self.assertEqual(len(result1), len(result2))
+
+    def test_default_length_value(self):
+        """
+        Testst that if pass nothing to the 'length'
+        argument, it sets the default value which is '10'.
+        """
+        result = unique_code_generator()
+
+        self.assertEqual(len(result), 10)
